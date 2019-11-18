39e803274ca1019bce58b6299aa045ebe4d3e1da
Elijah Manor <elijah.manor@gmail.com>
2019-11-17 21:39:25 -0600

    Update babel preset syntax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 require('@babel/register')({
-    presets: [['@babel/preset-env'], ['@babel/preset-react']],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
 })
 require('@babel/polyfill')
 require('./App.js')
