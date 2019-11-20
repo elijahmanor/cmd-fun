287d089becba5c1182c8f738180f511cdd1309ba
Elijah Manor <elijah.manor@gmail.com>
2019-11-20 10:36:46 -0800

    Added babel plugin to change modules to commonjs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 require('@babel/register')({
     presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ['@babel/plugin-transform-modules-commonjs'],
 })
 require('@babel/polyfill')
 require('./App.js')
